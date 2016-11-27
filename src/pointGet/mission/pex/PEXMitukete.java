f70eee093dcef3eaffb695df4ed63eee9de9e1b2
saitou <saitou@VAIOsaito>
2016-11-28 07:25:17 +0900

    何か変更

--- src/pointGet/mission/pex/PEXMitukete.java
+++ src/pointGet/mission/pex/PEXMitukete.java
@@ -1,6 +1,7 @@
 package pointGet.mission.pex;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
@@ -16,7 +17,28 @@ import pointGet.mission.Mission;
 public class PEXMitukete extends Mission {
 	final String url = "http://pex.jp/seal";
 	private boolean endFlag = false;
-
+	// PEX登録ページリンクのマップ
+	public final static Map<String, String> fieldMap = new HashMap<String, String>(){
+		{put("無料会員登録", "/point_actions/list/free_register");}
+		{put("キャンペーン応募", "/point_actions/list/campaign");}
+		{put("カード発行", "/point_actions/list/card");}
+		{put("口座開設", "/point_actions/list/open_account");}
+		{put("資料請求", "/point_actions/list/document_request");}
+		{put("査定・見積もり", "/point_actions/list/assessment");}
+		{put("相談", "/point_actions/list/consultation");}
+		{put("ゲーム起動", "/point_actions/list/start_up_game");}
+		{put("会員登録後のアクション", "/point_actions/list/register_and_action");}
+		{put("中古買取", "/point_actions/list/used_item_purchase");}
+		{put("その他", "/point_actions/list/free_point_action_other");}
+		{put("有料会員登録", "/point_actions/list/charge_register");}
+		{put("旅行", "/point_actions/list/travel");}
+		{put("エステ体験", "/point_actions/list/esthetic");}
+		{put("来店", "/point_actions/list/visit_store");}
+		{put("成約・借り入れ", "/point_actions/list/agree_contract");}
+		{put("ウォーターサーバー設置完了", "/point_actions/list/water_server");}
+		{put("その他", "/point_actions/list/charge_point_action_other");}
+		{put("全ての案件", "/point_actions/list/point_action");}
+	};
 	/**
 	 * @param logg
 	 */
@@ -92,4 +114,5 @@ public class PEXMitukete extends Mission {
 		}
 		return hint;
 	}
+	
 }
