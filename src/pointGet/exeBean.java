1e6a64c823005bfd4c3709ba1d171e324e7fd7a1
peter <peter@notepCmsaito>
2017-01-19 12:18:33 +0900

    個別呼び出し用にexeBeanを修正

--- src/pointGet/exeBean.java
+++ src/pointGet/exeBean.java
@@ -8,6 +8,114 @@ public class exeBean {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+//doTest();
+		String site = "";
+		String strFlag = "0";
+		String mission = "";
+		if (args.length > 0) {
+			mission = args[0];
+			if (args.length > 1) {
+				strFlag = args[1];
+			}
+		}
+		else {
+			// ■■■■■ECnavi■■■■■
+			//		mission = Define.strECNChinjyu;
+			//		mission = Define.strECNChirachi;
+			//		mission = Define.strECNClickBokin;
+			//		mission = Define.strECNDron;
+			//		mission = Define.strECNGaragara;
+			//		mission = Define.strECNNews;
+			//		mission = Define.strECNSearchBokin;
+			//		mission = Define.strECNTellmeWhich;
+			//		mission = Define.strECNWebSearche;
+			//		// ■■■■■げん玉■■■■■
+			//		mission = Define.strGENClickBanner;
+			//		mission = Define.strGENMorimoriKakome;
+			//		mission = Define.strGENPointStar;
+			//		mission = Define.strGENShindan;
+			//		mission = Define.strGENUranai;
+			//		// ■■■■■げっとま■■■■■
+			//		mission = Define.strGMYChirachi;
+			//		mission = Define.strGMYClickBanner;
+			//		mission = Define.strGMYShindan;
+			//		// ■■■■■i2i■■■■■
+			//		mission = Define.strI2ISeiza;
+			//		// ■■■■■moppy■■■■■
+			//		mission = Define.strMOPChyosatai;
+			//		mission = Define.strMOPClickBanner;
+			mission = Define.strMOPQuiz;
+			//		mission = Define.strMOPShindan;
+			//		mission = Define.strMOPUranai;
+			//		mission = Define.strMOPNanyoubi;
+			//		mission = Define.strMOPAnzan;
+			//		// ■■■■■osaifu■■■■■
+			//		mission = Define.strOSAClickBanner;
+			//		mission = Define.strOSAQuiz;
+			//		mission = Define.strOSAShindan;
+			//		mission = Define.strOSAUranai;
+			//		mission = Define.strOSANanyoubi;
+			//		mission = Define.strOSAAnzan;
+			//		// ■■■■■PEX■■■■■
+			//		mission = Define.strPEX4quiz;
+			//		mission = Define.strPEXAnswer;
+			//		mission = Define.strPEXChirachi;
+			//		mission = Define.strPEXClickBanner;
+			//		mission = Define.strPEXMekutte;
+			//		mission = Define.strPEXMitukete;
+			//		mission = Define.strPEXNews;
+			//		mission = Define.strPEXPectan;
+			//		mission = Define.strPEXSearch;
+			//		// ■■■■■rakuten■■■■■
+			//		mission = Define.strRINClickBanner;
+			//		// ■■■■■PointTown■■■■■
+			//		mission = Define.strPTOClickCorner;
+			//		mission = Define.strPTOKuji;
+			//		mission = Define.strPTODaily;
+			//		mission = Define.strPTOUranai;
+			strFlag = "0";
+		}
+
+		site = getSiteCode(mission);
+		WebClicker.sub(site, strFlag, mission);
+	}
+
+	private static String getSiteCode(String mission) {
+		String site = "";
+		switch (mission.substring(0, 3).toLowerCase()) {
+			case Define.PSITE_CODE_R01:
+			case Define.PSITE_CODE_RIN:
+				site = Define.PSITE_CODE_RIN;
+				break;
+			case Define.PSITE_CODE_PEX:
+				site = Define.PSITE_CODE_PEX;
+				break;
+			case Define.PSITE_CODE_PTO:
+				site = Define.PSITE_CODE_PTO;
+				break;
+			case Define.PSITE_CODE_GMY:
+				site = Define.PSITE_CODE_GMY;
+				break;
+			case Define.PSITE_CODE_GEN:
+				site = Define.PSITE_CODE_GEN;
+				break;
+			case Define.PSITE_CODE_ECN:
+				site = Define.PSITE_CODE_ECN;
+				break;
+			case Define.PSITE_CODE_MOP:
+				site = Define.PSITE_CODE_MOP;
+				break;
+			case Define.PSITE_CODE_OSA:
+				site = Define.PSITE_CODE_OSA;
+				break;
+			case Define.PSITE_CODE_I2I:
+				site = Define.PSITE_CODE_I2I;
+				break;
+		}
+		return site;
+	}
+
+	private static void doTest() {
 		// // TODO 数値の比較
 		// String str = "5 - 8 ÷ 7";
 		// // String str = text;
@@ -26,89 +134,5 @@ public class exeBean {
 		//// // logg.info("なんにちです [" + matchstr+"]");
 		//// }
 		// System.out.println("owari");
-
-		// String[] params = new String[]{"0",
-		// Define.PSITE_CODE_ECN, // ecnavi
-		// Define.PSITE_CODE_PEX, // pex
-		// Define.PSITE_CODE_GEN, // gendama
-		// Define.PSITE_CODE_GMY, // GetMoney
-		// Define.PSITE_CODE_RIN, // raktuten
-		// Define.PSITE_CODE_I2I, // i2i
-		// Define.PSITE_CODE_MOP, // moppi
-		// Define.PSITE_CODE_OSA, // osaifu
-		// Define.PSITE_CODE_PTO, // pointtown
-		// };
-
-		// if(args.length > 0) {
-		// params = args;
-		// }
-		// WebClicker.main(params);
-		WebClicker.sub(
-				// Define.PSITE_CODE_ECN, // ecnavi
-				// Define.PSITE_CODE_PEX, // pex
-				// Define.PSITE_CODE_GEN, // gendama
-				// Define.PSITE_CODE_GMY, // GetMoney
-				// Define.PSITE_CODE_RIN, // raktuten
-				// Define.PSITE_CODE_I2I, // i2i
-				// Define.PSITE_CODE_MOP, // moppi
-				// Define.PSITE_CODE_OSA, // osaifu
-				Define.PSITE_CODE_PTO, // pointtown
-				"0",
-				// ★★★★★★PointTown★★★★★★
-				//				Define.strPTOClickCorner
-//				Define.strPTOKuji
-		 Define.strPTODaily
-		//		 Define.strPTOUranai
-		// ★★★★★★GENDAMA★★★★★★
-		// Define.strGENClickBanner
-		// Define.strGENMorimoriKakome
-		// Define.strGENPointStar
-		// Define.strGENShindan
-		// Define.strGENUranai
-		// ★★★★★★I2I★★★★★★
-		// Define.strI2ISeiza
-		// ★★★★★★Moppy★★★★★★
-		// Define.strMOPChyosatai
-		// Define.strMOPClickBanner
-		// Define.strMOPQuiz
-		// Define.strMOPShindan
-		// Define.strMOPNanyoubi
-		// Define.strMOPUranai
-		// Define.strMOPAnzan
-		// ★★★★★★OSAIFU★★★★★★
-		// Define.strOSAClickBanner
-		// Define.strOSAQuiz
-		// Define.strOSAShindan
-		// Define.strOSAUranai
-		// Define.strOSAAnzan
-		// Define.strOSANanyoubi
-		// ★★★★★★PEX★★★★★★
-		// Define.strPEX4quiz
-		// Define.strPEXAnswer
-		// Define.strPEXChirachi
-		// Define.strPEXClickBanner
-		// Define.strPEXMekutte
-		// Define.strPEXMitukete
-		// Define.strPEXNews
-		// Define.strPEXPectan
-		// Define.strPEXSearch
-		// ★★★★★★楽天★★★★★★
-		// Define.strRINClickBanner
-		// ★★★★★★GetMoney★★★★★★
-		// Define.strGMYChirachi
-		// Define.strGMYClickBanner
-		// Define.strGMYShindan
-		// ★★★★★★ECnavi★★★★★★
-		// Define.strECNChinjyu
-		// Define.strECNChirachi
-		// Define.strECNClickBokin
-		// Define.strECNDron
-		// Define.strECNGaragara
-		// Define.strECNNews
-		// Define.strECNSearchBokin
-		// Define.strECNTellmeWhich
-		// Define.strECNWebSearche
-
-		);
 	}
 }
