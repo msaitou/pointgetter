dcd485fe2a5c5131cce68ee9c7cb144f2f536ee2
saitou <saitou@VAIOsaito>
2017-04-16 09:52:20 +0900

    ptoアンケート1パターン追加

--- src/pointGet/mission/pto/PTOPointResearch.java
+++ src/pointGet/mission/pto/PTOPointResearch.java
@@ -27,7 +27,6 @@ public class PTOPointResearch extends PTOBase {
 		driver = driverAtom;
 		driver.get(url);
 		int skip = 1;
-
 		Utille.sleep(3000);
 		while (true) {
 			selector = "td>span.promo_enq_bt";
@@ -39,103 +38,14 @@ public class PTOPointResearch extends PTOBase {
 				clickSleepSelector(eleList, targetIndex, 10000); // アンケートスタートページ
 				changeWindow(driver, wid);
 				selector = "div.ui-control.type-fixed>a.ui-button";
-				String sele3 = "div>button[type='submit']"; // 回答する surveyenk用
+				String sele3 = "div>button[type='submit']", // 回答する surveyenk用
+				sele1 = "div.question_btn>input[type='submit']";
 				if (isExistEle(driver, selector)) {
-					closeOtherWindow(driver);
-					clickSleepSelector(driver, selector, 4000);
-					// 回答開始
-					selector = "form>input.ui-button";
-					if (isExistEle(driver, selector)) {
-						clickSleepSelector(driver, selector, 4000);
-						String choiceSele = "label.ui-label-radio", seleNext2 = "div.fx-control>input.ui-button", seleSele = "select.ui-select", overLay = "div.overlay-popup a.button-close", noSele = "div.ui-item-no", titleSele = "h2.ui-item-title", checkSele = "label.ui-label-checkbox";
-						// 12問
-						for (int k = 1; k <= 13; k++) {
-							if (!isExistEle(driver, "div.overlay-popup[style*='display: none;'] a.button-close")
-									&& isExistEle(driver, overLay)) {
-								checkOverlay(driver, overLay, false);
-							}
-							if (isExistEle(driver, noSele)) {
-								String qNo = driver.findElement(By.cssSelector(noSele)).getText();
-								String qTitle = driver.findElement(By.cssSelector(titleSele)).getText();
-								logg.info(qNo + " " + qTitle);
-								int choiceNum = 0;
-								if (isExistEle(driver, choiceSele)) {
-									int choiceies = getSelectorSize(driver, choiceSele);
-									switch (k) {
-										case 1: // 性別
-										case 3: // 結婚
-											// 1問目は1：男
-											// 3問目は3：未婚
-											break;
-										case 5: // 職業
-										case 2: // 年齢
-											// 2問目は3：30代
-											if (choiceies > 2) {// 一応選択可能な範囲かをチェック
-												choiceNum = 2;
-											}
-											break;
-										default:
-											choiceNum = Utille.getIntRand(choiceies);
-									}
-									List<WebElement> eleList2 = driver.findElements(By.cssSelector(choiceSele));
-									if (isExistEle(eleList2, choiceNum)) {
-										// 選択
-										clickSleepSelector(eleList2.get(choiceNum), 3000);
-									}
-								}
-								else if (isExistEle(driver, checkSele)) {
-									int size4 = getSelectorSize(driver, checkSele);
-									choiceNum = Utille.getIntRand(size4);
-									List<WebElement> eleList2 = driver.findElements(By.cssSelector(checkSele));
-									if (isExistEle(eleList2, choiceNum)) {
-										// 選択
-										clickSleepSelector(eleList2.get(choiceNum), 3000);
-									}
-								}
-								else if (isExistEle(driver, seleSele)) {
-									Utille.sleep(2000);
-									int size3 = getSelectorSize(driver, seleSele + ">option");
-									String value = "";
-									if (qTitle.indexOf("居住区") >= 0) {
-										value = "4";
-									}
-									else {
-										choiceNum = Utille.getIntRand(size3);
-										value = driver.findElements(By.cssSelector(seleSele + ">option"))
-												.get(choiceNum).getAttribute("value");
-									}
-									Select selectList = new Select(driver.findElement(By.cssSelector(seleSele)));
-									selectList.selectByValue(value);
-									Utille.sleep(3000);
-								}
-							}
-							else {
-								break;
-							}
-							if (isExistEle(driver, seleNext2)) {
-								// 次へ
-								clickSleepSelector(driver, seleNext2, 4500);
-							}
-						}
-						Utille.sleep(2000);
-						if (!isExistEle(driver, "div.overlay-popup[style*='display: none;'] a.button-close")
-								&& isExistEle(driver, overLay)) {
-							checkOverlay(driver, overLay, false);
-						}
-						if (isExistEle(driver, selector)) {
-							// ポイント獲得
-							clickSleepSelector(driver, selector, 3000);
-						}
-						if (isExistEle(driver, overLay)) {
-							checkOverlay(driver, overLay, false);
-						}
-						String closeSele = "div.ui-control>a";
-						if (isExistEle(driver, closeSele)) {
-							// 閉じる
-							clickSleepSelector(driver, closeSele, 3000);
-						}
-						// point一覧に戻る
-					}
+					_answerPointResearch(sele3, wid);
+				}
+				else if (isExistEle(driver, sele1)) {
+					_answerAdserver(sele1, wid);
+					Utille.sleep(3000);
 				}
 				else if (isExistEle(driver, sele3)) {
 					closeOtherWindow(driver);
@@ -153,6 +63,109 @@ public class PTOPointResearch extends PTOBase {
 		}
 	}
 
+	/** 
+	 * 
+	 * @param sele3
+	 * @param wid
+	 */
+	private void _answerPointResearch(String sele3, String wid) {
+		closeOtherWindow(driver);
+		clickSleepSelector(driver, selector, 4000);
+		// 回答開始
+		selector = "form>input.ui-button";
+		if (isExistEle(driver, selector)) {
+			clickSleepSelector(driver, selector, 4000);
+			String choiceSele = "label.ui-label-radio", seleNext2 = "div.fx-control>input.ui-button", seleSele = "select.ui-select", overLay = "div.overlay-popup a.button-close", noSele = "div.ui-item-no", titleSele = "h2.ui-item-title", checkSele = "label.ui-label-checkbox";
+			// 12問
+			for (int k = 1; k <= 13; k++) {
+				if (!isExistEle(driver, "div.overlay-popup[style*='display: none;'] a.button-close")
+						&& isExistEle(driver, overLay)) {
+					checkOverlay(driver, overLay, false);
+				}
+				if (isExistEle(driver, noSele)) {
+					String qNo = driver.findElement(By.cssSelector(noSele)).getText();
+					String qTitle = driver.findElement(By.cssSelector(titleSele)).getText();
+					logg.info(qNo + " " + qTitle);
+					int choiceNum = 0;
+					if (isExistEle(driver, choiceSele)) {
+						int choiceies = getSelectorSize(driver, choiceSele);
+						switch (k) {
+							case 1: // 性別
+							case 3: // 結婚
+								// 1問目は1：男
+								// 3問目は3：未婚
+								break;
+							case 5: // 職業
+							case 2: // 年齢
+								// 2問目は3：30代
+								if (choiceies > 2) {// 一応選択可能な範囲かをチェック
+									choiceNum = 2;
+								}
+								break;
+							default:
+								choiceNum = Utille.getIntRand(choiceies);
+						}
+						List<WebElement> eleList2 = driver.findElements(By.cssSelector(choiceSele));
+						if (isExistEle(eleList2, choiceNum)) {
+							// 選択
+							clickSleepSelector(eleList2.get(choiceNum), 3000);
+						}
+					}
+					else if (isExistEle(driver, checkSele)) {
+						int size4 = getSelectorSize(driver, checkSele);
+						choiceNum = Utille.getIntRand(size4);
+						List<WebElement> eleList2 = driver.findElements(By.cssSelector(checkSele));
+						if (isExistEle(eleList2, choiceNum)) {
+							// 選択
+							clickSleepSelector(eleList2.get(choiceNum), 3000);
+						}
+					}
+					else if (isExistEle(driver, seleSele)) {
+						Utille.sleep(2000);
+						int size3 = getSelectorSize(driver, seleSele + ">option");
+						String value = "";
+						if (qTitle.indexOf("居住区") >= 0) {
+							value = "4";
+						}
+						else {
+							choiceNum = Utille.getIntRand(size3);
+							value = driver.findElements(By.cssSelector(seleSele + ">option"))
+									.get(choiceNum).getAttribute("value");
+						}
+						Select selectList = new Select(driver.findElement(By.cssSelector(seleSele)));
+						selectList.selectByValue(value);
+						Utille.sleep(3000);
+					}
+				}
+				else {
+					break;
+				}
+				if (isExistEle(driver, seleNext2)) {
+					// 次へ
+					clickSleepSelector(driver, seleNext2, 4500);
+				}
+			}
+			Utille.sleep(2000);
+			if (!isExistEle(driver, "div.overlay-popup[style*='display: none;'] a.button-close")
+					&& isExistEle(driver, overLay)) {
+				checkOverlay(driver, overLay, false);
+			}
+			if (isExistEle(driver, selector)) {
+				// ポイント獲得
+				clickSleepSelector(driver, selector, 3000);
+			}
+			if (isExistEle(driver, overLay)) {
+				checkOverlay(driver, overLay, false);
+			}
+			String closeSele = "div.ui-control>a";
+			if (isExistEle(driver, closeSele)) {
+				// 閉じる
+				clickSleepSelector(driver, closeSele, 3000);
+			}
+			// point一覧に戻る
+		}
+	}
+	
 	/**
 	 *
 	 * @param sele3
@@ -238,5 +251,75 @@ public class PTOPointResearch extends PTOBase {
 		//			driver.close();
 		// 最後に格納したウインドウIDにスイッチ
 		//		driver.switchTo().window(wid);
+	}
+	
+	private void _answerAdserver(String sele, String wid) {
+		String choiceSele, 
+				radioSele = "label.radiolabel",
+				overLay = "div#center-frame>img", 
+				noSele = "h2>span.q_number",
+				titleSele = "h2>span.q_text",
+				qTitle
+				;
+		clickSleepSelector(driver, sele, 4000);
+		Utille.sleep(4000);
+		for (int k = 1; k <= 20; k++) {
+			int choiceNum = 0;
+			qTitle = "";
+			choiceSele = "";
+			if (isExistEle(driver, noSele)) {
+				String qNo = driver.findElement(By.cssSelector(noSele)).getText();
+				qTitle = driver.findElement(By.cssSelector(titleSele)).getText();
+				logg.info(qNo + " " + qTitle);
+				if (isExistEle(driver, radioSele)) { // ラジオ
+					choiceSele = radioSele;
+				}
+				// 回答選択
+				if (radioSele.equals(choiceSele)
+//						|| checkboxSele.equals(choiceSele)
+							) {
+					int choiceies = getSelectorSize(driver, choiceSele);
+					if (qTitle.indexOf("あなたの性別を教えてください") >= 0) {
+						choiceNum = 0; // 1：男
+					}
+					else if (qTitle.indexOf("あなたの年齢を教えてください") >= 0) {
+						choiceNum = 3; // 4：30代
+					}
+					else if (qTitle.indexOf("あなたの職業を教えてください") >= 0) {
+						choiceNum = 3; // 3：会社員
+					}
+					else if (qTitle.indexOf("あなたのお住まいの都道府県を教えてください") >= 0) {
+						choiceNum = 3; // 3：関東
+					}
+					else {
+						choiceNum = Utille.getIntRand(choiceies);
+					}
+					List<WebElement> eleList2 = driver.findElements(By.cssSelector(choiceSele));
+					if (isExistEle(eleList2, choiceNum)) {
+						// 選択
+						clickSleepSelector(eleList2.get(choiceNum), 3000);
+						if (isExistEle(driver, sele)) {
+							clickSleepSelector(driver, sele, 6000);
+						}
+					}
+				}
+			}
+			else {
+				break;
+			}
+		}
+		Utille.sleep(5000);
+		// [style*='visibility: visible']
+		checkOverlay(driver, overLay, false);
+		if (!isExistEle(driver, "div#fade-layer[style*='display: none']")) {
+			checkOverlay(driver, overLay, false);
+		}
+		// close
+		String closeSele = "div.question_btn>input[name='submit']";
+		if (isExistEle(driver, closeSele)) {
+			clickSleepSelector(driver, closeSele, 4000);
+			driver.switchTo().window(wid);
+			driver.navigate().refresh();
+		}
 	}
 }
