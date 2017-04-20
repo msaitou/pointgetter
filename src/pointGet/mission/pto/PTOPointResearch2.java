4ef49ab00ab47ed255bdd7f6ffb7c7395401fb9b
saitou <saitou@VAIOsaito>
2017-04-21 07:34:16 +0900

    ptoリサーチ結構修正

--- src/pointGet/mission/pto/PTOPointResearch2.java
+++ src/pointGet/mission/pto/PTOPointResearch2.java
@@ -3,6 +3,8 @@ package pointGet.mission.pto;
 import java.util.List;
 import java.util.Map;
 
+import lombok.val;
+
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -31,40 +33,50 @@ public class PTOPointResearch2 extends PTOBase {
 		while (true) {
 			Utille.sleep(5000);
 			selector = "div#pt-enq1 td.frame04>a.promo_enq_bt";
-			List<WebElement> eleList = driver.findElements(By.cssSelector(selector));
-			int size = eleList.size();
-			int targetIndex = size - 1 - skip ; // 順番はサイト毎に変更可能だが、変数を使う
-			logg.info("size:"+size+" targetIndex:"+targetIndex+" skip:"+skip);
-			if (size > targetIndex && targetIndex > 0
-					&& isExistEle(eleList, targetIndex)) {
-				String wid = driver.getWindowHandle();
-				clickSleepSelector(eleList, targetIndex, 10000); // アンケートスタートページ
-				changeWindow(driver, wid);
-				selector = "div.ui-control.type-fixed>a.ui-button";
-				String sele3 = "div>button[type='submit']", // 回答する surveyenk用
-				sele1 = "div>input.btn_submit",
-				sele4 = "div>input[type='submit']",
-				sele2 = "div.page-content-button>input.button.btn-next";
-//				if (isExistEle(driver, selector)) {
-//					_answerPointResearch(sele3, wid);
-//				}
-//				else if (isExistEle(driver, sele1)) {
-//					_answerAdserver(sele1, wid);
-//					Utille.sleep(3000);
-//				}
-//				else 
-					if (isExistEle(driver, sele3)) {
-					_answerSurveyEnk(sele3, "");
-				}
-				else if (isExistEle(driver, sele2)) {
-					_answerKotsuta(sele2, wid);
-				}
-				else if (isExistEle(driver, sele1)) {
-					_answerInfopanel(sele1, wid);
-				}
-				else {
-					driver.switchTo().frame(0);
-					if (isExistEle(driver, sele4)) {
+			if (isExistEle(driver, selector)) {
+				List<WebElement> eleList = driver.findElements(By.cssSelector(selector));
+				int size = eleList.size();
+				int targetIndex = size - 1 - skip ; // 順番はサイト毎に変更可能だが、変数を使う
+				logg.info("size:"+size+" targetIndex:"+targetIndex+" skip:"+skip);
+				if (size > targetIndex && targetIndex > 0
+						&& isExistEle(eleList, targetIndex)) {
+					String wid = driver.getWindowHandle();
+					clickSleepSelector(eleList, targetIndex, 10000); // アンケートスタートページ
+					changeWindow(driver, wid);
+//					selector = "div.ui-control.type-fixed>a.ui-button";
+					String sele3 = "div.enq-submit>button[type='submit']", // 回答する surveyenk用
+					sele1 = "div>input.btn_submit",
+					sele4 = "div>input[type='submit']",
+					sele4_ = "#iframe",
+					sele5 = "div#shindan",
+					sele7 = "div.btn>button[type='submit']",
+					sele6 = "form>input.next_bt",
+					sele2 = "div.page-content-button>input.button.btn-next";
+	//				if (isExistEle(driver, selector)) {
+	//					_answerPointResearch(sele3, wid);
+	//				}
+	//				else if (isExistEle(driver, sele1)) {
+	//					_answerAdserver(sele1, wid);
+	//					Utille.sleep(3000);
+	//				}
+	//				else 
+						if (isExistEle(driver, sele3)) {
+						_answerSurveyEnk(sele3, wid);
+					}
+					else if (isExistEle(driver, sele2)) {
+						_answerKotsuta(sele2, wid);
+					}
+					else if (isExistEle(driver, sele1)) {
+						_answerInfopanel(sele1, wid);
+					}
+					else if (isExistEle(driver, sele5)) {
+						_answerShindan(sele5, wid);
+					}
+					else if (isExistEle(driver, sele6)) {
+						_answerColum(sele6, wid);
+					}
+					else if (isExistEle(driver, sele4_)) {
+						driver.switchTo().frame(0);
 						_answerShopping(sele4, wid);
 					}
 					else {
@@ -73,13 +85,181 @@ public class PTOPointResearch2 extends PTOBase {
 						driver.switchTo().window(wid);
 					}
 				}
-			}
-			else {
-				break;
+				else {
+					break;
+				}
 			}
 		}
 	}
-	
+	private void _answerColum(String sele6, String wid) {
+		String seleNextb2 = "form>input[type='image']";
+		String seleNextb3 = "form>input[alt='next']";
+		String overLay = "div#interstitial[style*='display: block']>div>div#inter-close";
+		if (isExistEle(driver, sele6)) {
+			scrolledPage(driver, sele6);
+			clickSleepSelector(driver, sele6, 4000); // 遷移　問開始
+			for (int g = 0; g < 2; g++) {
+				if (isExistEle(driver, seleNextb2)) {
+					scrolledPage(driver, seleNextb2);
+					clickSleepSelector(driver, seleNextb2, 4000); // 遷移　問開始するよ
+				}
+			}
+			Utille.sleep(7000);
+			// 6page
+			for (int g = 0; g < 6; g++) {
+				if (isExistEle(driver, seleNextb3)) {
+					scrolledPage(driver, seleNextb3);
+					clickSleepSelector(driver, seleNextb3, 13000); // 遷移　問開始するよ
+				}
+			}
+			checkOverlay(driver, overLay, false);
+			if (isExistEle(driver, seleNextb2)) {
+				scrolledPage(driver, seleNextb2);
+				clickSleepSelector(driver, seleNextb2, 5000); // 遷移　問開始するよ
+			}
+			String choiceSele = "input[type='radio']";
+			String seleNext2 = "div>input.enquete_nextbt";
+			String seleNext3 = "div>input.enquete_nextbt_2";
+			String seleSele = "form.menu>select";
+			// 6問
+			for (int k = 1; k <= 6; k++) {
+				checkOverlay(driver, overLay, false);
+				int choiceNum = 0;
+				if (isExistEle(driver, choiceSele)) {
+					int choiceies = getSelectorSize(driver, choiceSele);
+					switch (k) {
+						case 1:
+							// 1問目は1：男
+							break;
+						case 2:
+						case 3:
+							// 2問目は3：30代
+							// 3問目は3：会社員
+							if (choiceies > 2) {// 一応選択可能な範囲かをチェック
+								choiceNum = 2;
+							}
+							break;
+						default:
+							choiceNum = Utille.getIntRand(choiceies);
+					}
+					List<WebElement> eleList2 = driver.findElements(By.cssSelector(choiceSele));
+					if (isExistEle(eleList2, choiceNum)) {
+						// 選択
+						clickSleepSelector(eleList2.get(choiceNum), 3500);
+						if (isExistEle(driver, seleNext2)) {
+							// 次へ
+							scrolledPage(driver, seleNext2);
+							clickSleepSelector(driver, seleNext2, 4000);
+						}
+					}
+				}
+				else if (isExistEle(driver, seleSele)) {
+					int size3 = getSelectorSize(driver, seleSele + ">option");
+					choiceNum = Utille.getIntRand(size3);
+					String value = driver.findElements(By.cssSelector(seleSele + ">option"))
+							.get(choiceNum).getAttribute("value");
+					Select selectList = new Select(driver.findElement(By.cssSelector(seleSele)));
+					//										selectList.deselectByIndex(choiceNum);
+					selectList.selectByValue(value);
+					if (isExistEle(driver, seleNext3)) {
+						// 次へ
+						scrolledPage(driver, seleNext3);
+						clickSleepSelector(driver, seleNext3, 4000);
+					}
+				}
+				else {
+					break;
+				}
+			}
+			for (int ii = 0; ii<2;ii++) {
+				checkOverlay(driver, overLay, false);
+				if (isExistEle(driver, seleNextb2)) {
+					clickSleepSelector(driver, seleNextb2, 4000); // 遷移　
+				}
+			}
+			String finishSele = "div#again_bt>a>img";
+			checkOverlay(driver, overLay, false);
+			if (isExistEle(driver, finishSele)) {
+				clickSleepSelector(driver, finishSele, 5000); // 遷移
+				// アラートをけして
+				val alert = driver.switchTo().alert();
+				alert.accept();
+				Utille.sleep(5000);
+			}
+			String closeSele = "input.btn_close_en";
+			if (isExistEle(driver, closeSele)) {
+				clickSleepSelector(driver, closeSele, 4000);
+				driver.switchTo().window(wid);
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @param sele5
+	 * @param wid
+	 */
+	private void _answerShindan(String sele5, String wid) {
+		// 表示される結果によって処理を分ける
+//		logg.info(mName + " " + ".診断だ! " + j + " click!!");
+		selector = "a.submit-btn";// 次へ
+		if (isExistEle(driver, selector)) {
+			clickSleepSelector(driver, selector, 5500); // 遷移
+			if (isExistEle(driver, selector)) {
+				scrolledPage(driver, selector);
+				clickSleepSelector(driver, selector, 7000); // 遷移
+				if (isExistEle(driver, "div[data-qid]")) {
+					int qSize = getSelectorSize(driver, "div[data-qid]"); // 選択肢の数を数える
+					for (int i = 0; i < qSize+5; i++) {
+						selector = "div[data-qid][class=''] label";
+						if (isExistEle(driver, selector)) {
+							int size = getSelectorSize(driver, selector); // 選択肢の数を数える
+							int ran1 = Utille.getIntRand(size);
+							if (isExistEle(driver.findElements(By.cssSelector(selector)).get(ran1))) {
+								driver.findElements(By.cssSelector(selector)).get(ran1).click(); // 選択
+								Utille.sleep(4000);
+
+								// end-btn が出たら終了
+								String none = "[style*='display: none']";
+								String nextSelector = "div.actionBar>a.next-btn";
+								String endSelector = "div.actionBar>a.end-btn";
+								if (isExistEle(driver, nextSelector)
+										&& isExistEle(driver, endSelector + none)) {
+									scrolledPage(driver, nextSelector);
+									clickSleepSelector(driver, nextSelector, 2000); // 遷移
+								} else if (isExistEle(driver, endSelector)
+										&& isExistEle(driver, nextSelector + none)) {
+									scrolledPage(driver, endSelector);
+									clickSleepSelector(driver, endSelector, 4000); // 遷移
+									// 抜けたら
+									// span#end-btn-area>a.end-btn
+									// をクリック
+									logg.info("neukata?");
+//									selector = "span#end-btn-area>a.end-btn";
+//									if (isExistEle(driver, selector)) {
+//										scrolledPage(driver, selector);
+//										clickSleepSelector(driver, selector, 2000); // 遷移
+
+										selector = "div.col-xs-12>a.btn-danger";
+										if (isExistEle(driver, selector)) {
+											clickSleepSelector(driver, selector, 4000); // 遷移
+										}
+										String closeSele = "input.btn_close_en";
+										if (isExistEle(driver, closeSele)) {
+											clickSleepSelector(driver, closeSele, 4000);
+										}
+										driver.switchTo().window(wid);
+										break;
+//									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
 	/**
 	 *
 	 * @param sele3
@@ -125,7 +305,7 @@ public class PTOPointResearch2 extends PTOBase {
 					else if (qTitle.indexOf("あなたのご職業") >= 0) {
 						choiceNum = 2; // 2：会社員
 					}
-					else if (qTitle.indexOf("あなたのお住まい") >= 0) {
+					else if (qTitle.indexOf("あなたのお住まいを") >= 0) {
 						choiceNum = 2; // 2：関東
 					}
 					else {
@@ -136,13 +316,15 @@ public class PTOPointResearch2 extends PTOBase {
 						// 選択
 						clickSleepSelector(eleList2.get(choiceNum), 3000);
 						String seleNe = "div.btn_next>input[type='submit']";
+						scrolledPage(driver, seleNe);
 						WebElement element = driver.findElement(By.cssSelector(seleNe));
 					    ((JavascriptExecutor) driver).executeScript(
 					      "arguments[0].scrollIntoView(true);",
 					      element);
 					    JavascriptExecutor js = (JavascriptExecutor) driver;
 					    js.executeScript("javascript:window.scrollBy(0,-80)");//scrollIntoView(true)だけだとスクロールしすぎるので、少し戻す
-						if (isExistEle(driver, seleNe)) {
+						
+					    if (isExistEle(driver, seleNe)) {
 							clickSleepSelector(driver, seleNe, 6000);
 						}
 					}
@@ -457,13 +639,16 @@ public class PTOPointResearch2 extends PTOBase {
 		// 回答開始
 		clickSleepSelector(driver, sele3, 3000);
 		String qTitleSele = "div.question-label", qTitle = "", radioSele = "label.item-radio", checkboxSele = "label.item-checkbox", choiceSele = "", seleSele = "select.mdl-textfield__input";
-		for (int k = 1; k <= 15; k++) {
+		for (int k = 1; k <= 18; k++) {
 			int choiceNum = 0;
 			qTitle = "";
 			choiceSele = "";
 			if (isExistEle(driver, qTitleSele)) {
 				qTitle = driver.findElement(By.cssSelector(qTitleSele)).getText();
 				logg.info("[" + qTitle + "]");
+			}
+			else {
+				break;
 			}
 			if (isExistEle(driver, radioSele)) { // ラジオ
 				choiceSele = radioSele;
@@ -477,21 +662,16 @@ public class PTOPointResearch2 extends PTOBase {
 			if (radioSele.equals(choiceSele)
 					|| checkboxSele.equals(choiceSele)) {
 				int choiceies = getSelectorSize(driver, choiceSele);
-				switch (k) {
-					case 13: // 性別
-						//									case 3: // 結婚
-						// 13問目は1：男
-						// 3問目は3：未婚
-						break;
-					//									case 5: // 職業
-					case 14: // 年齢
-						// 2問目は3：30代
-						if (choiceies > 3) {// 一応選択可能な範囲かをチェック
-							choiceNum = 3;
-						}
-						break;
-					default:
-						choiceNum = Utille.getIntRand(choiceies);
+				if (qTitle.indexOf("あなたの性別を") >= 0) {
+					// 1：男
+				}
+				else if (qTitle.indexOf("あなたの年齢を") >= 0) {
+					if (choiceies > 3) {// 一応選択可能な範囲かをチェック
+						choiceNum = 3;
+					}
+				}
+				else {
+					choiceNum = Utille.getIntRand(choiceies);
 				}
 				List<WebElement> eleList2 = driver.findElements(By.cssSelector(choiceSele));
 				if (isExistEle(eleList2, choiceNum)) {
@@ -605,5 +785,18 @@ public class PTOPointResearch2 extends PTOBase {
 			driver.switchTo().window(wid);
 			driver.navigate().refresh();
 		}
+	}
+	
+	/**
+	 * 
+	 * @param driver
+	 * @param seleNe
+	 */
+	public void scrolledPage(WebDriver driver, String seleNe) {
+		WebElement ele = driver.findElement(By.cssSelector(seleNe));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", ele);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		//scrollIntoView(true)だけだとスクロールしすぎるので、少し戻す
+		js.executeScript("javascript:window.scrollBy(0,-80)");
 	}
 }
