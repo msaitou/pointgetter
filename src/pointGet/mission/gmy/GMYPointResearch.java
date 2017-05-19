81c0e87269c0caa4ac945d38e4c1297ef1ea473b
saitou <saitou@VAIOsaito>
2017-05-19 22:21:45 +0900

    Gmy何か直した

--- src/pointGet/mission/gmy/GMYPointResearch.java
+++ src/pointGet/mission/gmy/GMYPointResearch.java
@@ -101,11 +101,14 @@ public class GMYPointResearch extends GMYBase {
           // $('iframe').contents().find("div>input[type='submit']")
           Shopping.answer(driver, sele4, wid);
         }
-        else if (cUrl.indexOf("column-enquete") >= 0
+        else if (
+            (cUrl.indexOf("column-enquete") >= 0
+            || cUrl.indexOf("beautynail-design.com") >= 0)
             && isExistEle(driver, sele6)) {
           Colum.answer(driver, sele6, wid);
         }
-        else if (cUrl.indexOf("photo-enquete") >= 0
+        else if ((cUrl.indexOf("photo-enquete") >= 0
+            || cUrl.indexOf("cosmetic-brand.com") >= 0)
             && isExistEle(driver, sele8)) {
           PhotoEnk.answer(driver, sele8, wid);
         }
