d00c910411689b2c59df7d29f94061c8a9510d75
peter <peter@notepCmsaito>
2017-07-08 17:58:48 +0900

    osaファーム更新

--- src/pointGet/mission/osa/OSAFarmEnk.java
+++ src/pointGet/mission/osa/OSAFarmEnk.java
@@ -40,37 +40,114 @@ public class OSAFarmEnk extends OSABase {
 
   @Override
   public void privateMission(WebDriver driverAtom) {
+//    driver = driverAtom;
+//    driver.get(url);
+//    selector = "li>a>img[alt='お財布ファーム']";
+//    String enkLinkSele = "li#enqueteNav>a>*", //
+//        a = "";
+//    if (isExistEle(driver, selector)) {
+//      clickSleepSelector(driver, selector, 4000); // 遷移
+//      changeCloseWindow(driver);
+//      if (isExistEle(driver, enkLinkSele)) {
+//        clickSleepSelector(driver, enkLinkSele, 4000); // 遷移
+//        int skip = 1;
+//        selector = "div.enqueteContainer a[href] dd.title";
+//        String sele1_ = "iframe.question_frame", //
+//            sele1 = "form>input[type='submit']", //
+//            sele3 = "form>input[type='submit']", //
+//            sele9 = "a.start__button", //
+//            overlaySele = "div#meerkat-wrap div#overlay img.ad_close", //
+//            b = "";
+//        while (true) {
+//          if (!isExistEle(driver, selector)) {
+//            break;
+//          }
+//          List<WebElement> eleList = driver.findElements(By.cssSelector(selector));
+//          int size = eleList.size(), targetIndex = skip;
+//          if (size > targetIndex && isExistEle(eleList, targetIndex)) {
+//            String wid = driver.getWindowHandle();
+//            Utille.scrolledPage(driver, eleList.get(targetIndex));
+//            clickSleepSelector(eleList, targetIndex, 3000); // アンケートスタートページ
+//            changeWindow(driver, wid);
+//            String cUrl = driver.getCurrentUrl();
+//            logg.info("url[" + cUrl + "]");
+//            if (isExistEle(driver, sele9)) {
+//              Tasuuketu.answer(driver, sele9, wid);
+//              skip++;
+//            }
+//            else if (cUrl.indexOf("ad/enq/") >= 0
+//                && isExistEle(driver, sele1_)) {
+//              // $('iframe').contents().find("div>input[type='submit']")
+//              AdEnq.answer(driver, sele1, wid);
+//            }
+//            else if ((cUrl.indexOf("diagnosis.media-ad.jp/") >= 0
+//                || cUrl.indexOf("enquetter.com/question") >= 0)
+//                && isExistEle(driver, sele3)) {
+//              AdShindan.answer(driver, sele3, wid);
+//              skip++;
+//            }
+//            else {
+//              skip++;
+//              driver.close();
+//              driver.switchTo().window(wid);
+//            }
+//            driver.navigate().refresh();
+//            Utille.sleep(5000);
+//          }
+//          else {
+//            break;
+//          }
+//        }
+//        String stampSele = "a.suggest-surveys";
+//        if (isExistEle(driver, stampSele)) {
+//          clickSleepSelector(driver, stampSele, 4000); // 遷移
+//        }
+//      }
+//    }
+
     driver = driverAtom;
     driver.get(url);
     selector = "li>a>img[alt='お財布ファーム']";
-    String enkLinkSele = "li#enqueteNav>a>*", //
-        a = "";
-    if (isExistEle(driver, selector)) {
-      clickSleepSelector(driver, selector, 4000); // 遷移
+    String enkLinkSele = "li>a>img[alt='お財布ファーム']", //
+    a = "";
+
+    if (isExistEle(driver, enkLinkSele)) {
+      clickSleepSelector(driver, enkLinkSele, 4000); // 遷移
       changeCloseWindow(driver);
-      if (isExistEle(driver, enkLinkSele)) {
-        clickSleepSelector(driver, enkLinkSele, 4000); // 遷移
+      driver.get("http://farm.osaifu.com/square/votes");
+      for (int k = 0; k < 3; k++) {
+        if (k == 1) {
+          driver.get("http://farm.osaifu.com/square/diagnoses");
+        }
+        else if (k == 2) {
+          driver.get("http://farm.osaifu.com/square/surveys");
+        }
+
+        Utille.sleep(3000);
+        selector = "div.enqueteBox a[href]>dl";
         int skip = 1;
-        selector = "div.enqueteContainer a[href] dd.title";
         String sele1_ = "iframe.question_frame", //
-            sele1 = "form>input[type='submit']", //
-            sele3 = "form>input[type='submit']", //
-            sele9 = "a.start__button", //
-            overlaySele = "div#meerkat-wrap div#overlay img.ad_close", //
-            b = "";
+        sele1 = "form>input[type='submit']", //
+        sele3 = "form>input[type='submit']", //
+        sele9 = "a.start__button", overlaySele = "div#meerkat-wrap div#overlay img.ad_close", //
+        sele6 = "form>input.next_bt", // コラム用
+        b = "";
         while (true) {
+          checkOverlay(driver, overlaySele, false);
           if (!isExistEle(driver, selector)) {
             break;
           }
           List<WebElement> eleList = driver.findElements(By.cssSelector(selector));
-          int size = eleList.size(), targetIndex = skip;
-          if (size > targetIndex && isExistEle(eleList, targetIndex)) {
+          int size = eleList.size(), targetIndex = size - skip;
+          if (size > targetIndex &&
+              targetIndex >= 0 && isExistEle(eleList, targetIndex)) {
             String wid = driver.getWindowHandle();
             Utille.scrolledPage(driver, eleList.get(targetIndex));
             clickSleepSelector(eleList, targetIndex, 3000); // アンケートスタートページ
             changeWindow(driver, wid);
+            Utille.sleep(3000);
             String cUrl = driver.getCurrentUrl();
-            logg.info("url[" + cUrl + "]");
+            logg.info("cUrl[" + cUrl + "]");
             if (isExistEle(driver, sele9)) {
               Tasuuketu.answer(driver, sele9, wid);
               skip++;
@@ -78,14 +155,24 @@ public class OSAFarmEnk extends OSABase {
             else if (cUrl.indexOf("ad/enq/") >= 0
                 && isExistEle(driver, sele1_)) {
               // $('iframe').contents().find("div>input[type='submit']")
-              AdEnq.answer(driver, sele1, wid);
+              if (!AdEnq.answer(driver, sele1, wid)) {
+                break;
+              }
             }
-            else if ((cUrl.indexOf("diagnosis.media-ad.jp/") >= 0
+            else if (
+                (cUrl.indexOf("diagnosis.media-ad.jp/") >= 0
                 || cUrl.indexOf("enquetter.com/question") >= 0)
                 && isExistEle(driver, sele3)) {
               AdShindan.answer(driver, sele3, wid);
               skip++;
             }
+//            else if ((cUrl.indexOf("column-enquete") >= 0
+//                || cUrl.indexOf("beautynail-design.com") >= 0
+//                || cUrl.indexOf("fashion-cosmelife.com") >= 0
+//                )
+//                && isExistEle(driver, sele6)) {
+//              Colum.answer(driver, sele6, wid);
+//            }
             else {
               skip++;
               driver.close();
@@ -98,11 +185,12 @@ public class OSAFarmEnk extends OSABase {
             break;
           }
         }
-        String stampSele = "a.suggest-surveys";
-        if (isExistEle(driver, stampSele)) {
-          clickSleepSelector(driver, stampSele, 4000); // 遷移
-        }
+      }
+      String stampSele = "a.suggest-surveys";
+      if (isExistEle(driver, stampSele)) {
+        clickSleepSelector(driver, stampSele, 4000); // 遷移
       }
     }
+
   }
 }
