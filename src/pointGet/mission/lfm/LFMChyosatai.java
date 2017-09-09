d3cf2a7d3247dcaf7661cd1a2794088ac39f9603
peter <peter@notepCmsaito>
2017-09-09 20:51:17 +0900

    調査隊のポイント交換実装

--- src/pointGet/mission/lfm/LFMChyosatai.java
+++ src/pointGet/mission/lfm/LFMChyosatai.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import pointGet.common.Utille;
 
@@ -29,76 +30,47 @@ public class LFMChyosatai extends LFMBase {
     driver.get(url);
     String overlaySelector = "div#popup[style*='display: block'] a.modal_close";
     String footBnrSelector = "div.foot-bnr a.close>span", //
-    footBnrNoneSele = "div.foot-bnr[style*='display :none'] a.close>span";
-//    if (isExistEle(driver, selector)) {
-//      clickSleepSelector(driver, selector, 4000); // 遷移
-//      changeCloseWindow(driver);
-      for (int j = 0; j < 6; j++) {
-        driver.get(url);
-//        selector = "p.tokimeki";
-        String selector = "dd>a>img[alt='トキメキ調査隊']";
-        if (isExistEle(driver, selector, false)) {
-          clickSleepSelector(driver, selector, 4000); // 遷移
-          changeCloseWindow(driver);
+        footBnrNoneSele = "div.foot-bnr[style*='display :none'] a.close>span";
+    //    if (isExistEle(driver, selector)) {
+    //      clickSleepSelector(driver, selector, 4000); // 遷移
+    //      changeCloseWindow(driver);
+    for (int j = 0; j < 6; j++) {
+      driver.get(url);
+      //        selector = "p.tokimeki";
+      String selector = "dd>a>img[alt='トキメキ調査隊']";
+      if (isExistEle(driver, selector, false)) {
+        clickSleepSelector(driver, selector, 4000); // 遷移
+        changeCloseWindow(driver);
+        checkOverlay(driver, overlaySelector);
+        if (isExistEle(driver, footBnrSelector)
+            && !isExistEle(driver, footBnrNoneSele, false)) {
+          checkOverlay(driver, footBnrSelector);
+        }
+
+        String noEntrySele = "div.thumbnail span.icon-noentry";
+        String entrySele = "div.thumbnail span.icon-entry";
+        if (isExistEle(driver, noEntrySele)) {
+          clickSleepSelector(driver, noEntrySele, 3000); // 遷移
           checkOverlay(driver, overlaySelector);
           if (isExistEle(driver, footBnrSelector)
               && !isExistEle(driver, footBnrNoneSele, false)) {
             checkOverlay(driver, footBnrSelector);
           }
 
-          String noEntrySele = "div.thumbnail span.icon-noentry";
-          String entrySele = "div.thumbnail span.icon-entry";
-          if (isExistEle(driver, noEntrySele)) {
-            clickSleepSelector(driver, noEntrySele, 3000); // 遷移
+          selector = "div.thumb-start div.btn>a";
+          if (isExistEle(driver, selector)) {
+            clickSleepSelector(driver, selector, 4000); // 遷移
             checkOverlay(driver, overlaySelector);
-            if (isExistEle(driver, footBnrSelector)
-                && !isExistEle(driver, footBnrNoneSele, false)) {
-              checkOverlay(driver, footBnrSelector);
+            checkOverlay(driver, footBnrSelector);
+            String finshSele = "div.finish-area";
+            // otukare!
+            if (isExistEle(driver.findElements(By.cssSelector(finshSele)))) {
+              break;
             }
-
-            selector = "div.thumb-start div.btn>a";
-            if (isExistEle(driver, selector)) {
-              clickSleepSelector(driver, selector, 4000); // 遷移
-              checkOverlay(driver, overlaySelector);
-              checkOverlay(driver, footBnrSelector);
-              String finshSele = "div.finish-area";
-              // otukare!
-              if (isExistEle(driver.findElements(By.cssSelector(finshSele)))) {
-                break;
-              }
-              selector = "span.icon-arrow";
-              for (int i = 0; i < 10; i++) {
-                int ran = Utille.getIntRand(2);
-                if (isExistEle(driver.findElements(By.cssSelector(selector)), ran)) {
-                  clickSleepSelector(driver.findElements(By.cssSelector(selector)), ran, 3000);
-                  checkOverlay(driver, overlaySelector);
-                  if (isExistEle(driver, footBnrSelector)
-                      && !isExistEle(driver, footBnrNoneSele, false)) {
-                    checkOverlay(driver, footBnrSelector);
-                  }
-                }
-              }
-            }
-          }
-          else if (isExistEle(driver, entrySele)) {
-            clickSleepSelector(driver, entrySele, 3000); // 遷移
-            checkOverlay(driver, overlaySelector);
-            if (isExistEle(driver, footBnrSelector)
-                && !isExistEle(driver, footBnrNoneSele, false)) {
-              checkOverlay(driver, footBnrSelector);
-            }
-            //          selector = "div.btn>a";
             selector = "span.icon-arrow";
             for (int i = 0; i < 10; i++) {
-              String finshSele = "div.finish-area";
-              // otukare!
-              if (isExistEle(driver.findElements(By.cssSelector(finshSele)))) {
-                break;
-              }
               int ran = Utille.getIntRand(2);
-              Utille.sleep(2000);
-              if (isExistEle(driver.findElements(By.cssSelector(selector)))
-                  && isExistEle(driver.findElements(By.cssSelector(selector)), ran)) {
+              if (isExistEle(driver.findElements(By.cssSelector(selector)), ran)) {
                 clickSleepSelector(driver.findElements(By.cssSelector(selector)), ran, 3000);
                 checkOverlay(driver, overlaySelector);
                 if (isExistEle(driver, footBnrSelector)
@@ -108,11 +80,65 @@ public class LFMChyosatai extends LFMBase {
               }
             }
           }
-          else {
-            j = 6;
+        }
+        else if (isExistEle(driver, entrySele)) {
+          clickSleepSelector(driver, entrySele, 3000); // 遷移
+          checkOverlay(driver, overlaySelector);
+          if (isExistEle(driver, footBnrSelector)
+              && !isExistEle(driver, footBnrNoneSele, false)) {
+            checkOverlay(driver, footBnrSelector);
+          }
+          //          selector = "div.btn>a";
+          selector = "span.icon-arrow";
+          for (int i = 0; i < 10; i++) {
+            String finshSele = "div.finish-area";
+            // otukare!
+            if (isExistEle(driver.findElements(By.cssSelector(finshSele)))) {
+              break;
+            }
+            int ran = Utille.getIntRand(2);
+            Utille.sleep(2000);
+            if (isExistEle(driver.findElements(By.cssSelector(selector)))
+                && isExistEle(driver.findElements(By.cssSelector(selector)), ran)) {
+              clickSleepSelector(driver.findElements(By.cssSelector(selector)), ran, 3000);
+              checkOverlay(driver, overlaySelector);
+              if (isExistEle(driver, footBnrSelector)
+                  && !isExistEle(driver, footBnrNoneSele, false)) {
+                checkOverlay(driver, footBnrSelector);
+              }
+            }
           }
         }
-//      }
+        else {
+          j = 6;
+        }
+      }
+    }
+    String exchangeSele = "span.btn-exchange>a", //
+        exchageListSele = "select[name='point']",
+        doExchangeSele = "div.btn-area>button[type='submit']",
+        sele = "div.game_btn>div.icon>img[alt='トキメキ調査隊']";
+    ;
+    driver.get(url);
+    if (isExistEle(driver, sele)) {
+      clickSleepSelector(driver, sele, 4000); // 遷移
+      changeCloseWindow(driver);
+      if (isExistEle(driver, exchangeSele)) {
+        clickSleepSelector(driver, exchangeSele, 6000);
+        if (isExistEle(driver, exchageListSele)) {
+          int size = getSelectorSize(driver, exchageListSele + ">option");
+          String value = driver.findElements(By.cssSelector(exchageListSele + ">option"))
+              .get(size - 1).getAttribute("value");
+          Select selectList = new Select(driver.findElement(By.cssSelector(exchageListSele)));
+          selectList.selectByValue(value); // 交換ポイントを選択
+          Utille.sleep(3000);
+          for (int i = 0; i < 2; i++) {
+            if (isExistEle(driver, doExchangeSele)) {
+              clickSleepSelector(driver, doExchangeSele, 6000); // i=1 交換する　i=2 本当に
+            }
+          }
+        }
+      }
     }
   }
 }
