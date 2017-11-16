6904e222bc5aa51c5ce42fae47d3b529a15d0e3a
saitou <saitou@VAIOsaito.mshome.net>
2017-11-17 08:24:06 +0900

    dron調整

--- src/pointGet/mission/ecn/ECNDron.java
+++ src/pointGet/mission/ecn/ECNDron.java
@@ -12,28 +12,28 @@ import pointGet.common.Utille;
  *
  */
 public class ECNDron extends ECNBase {
-	final String url = "";
+  final String url = "";
 
-	/**
-	 * @param log
-	 */
-	public ECNDron(Logger log, Map<String, String> cProps) {
-		super(log, cProps, "ドロンバナークリック2種");
-	}
+  /**
+   * @param log
+   */
+  public ECNDron(Logger log, Map<String, String> cProps) {
+    super(log, cProps, "ドロンバナークリック2種");
+  }
 
-	@Override
-	public void privateMission(WebDriver driver) {
-		String[] dronUrlList = { "http://ecnavi.jp/", "http://ecnavi.jp/shopping/#doron" };
-		selector = "div#doron a.item>img";
-		for (int i = 0; i < dronUrlList.length; i++) {
-			driver.get(dronUrlList[i]);
-			Utille.sleep(2000);
-			if (isExistEle(driver, "div.js_anime.got")) {
-				logg.warn(mName + i + "]獲得済み");
-			}
-			else if (isExistEle(driver, selector)) {
-				clickSleepSelector(driver, selector, 2000);
-			}
-		}
-	}
+  @Override
+  public void privateMission(WebDriver driver) {
+    String[] dronUrlList = { "http://ecnavi.jp/", "http://ecnavi.jp/shopping/#doron" };
+    selector = "div#doron a.item>img";
+    for (int i = 0; i < dronUrlList.length; i++) {
+      driver.get(dronUrlList[i]);
+      Utille.sleep(2000);
+      if (isExistEle(driver, "div.js_anime.got")) {
+        logg.warn(mName + i + "]獲得済み");
+      }
+      else if (isExistEle(driver, selector)) {
+        clickSleepSelector(driver, selector, 2000);
+      }
+    }
+  }
 }
