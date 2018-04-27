c6d8efd61f984c21ee779c1b2fb7220e3eeb7d17
user <user@saitoNote>
2018-04-27 17:33:39 +0900

    osaファーム修正

--- src/pointGet/mission/osa/OSAFarmEnk.java
+++ src/pointGet/mission/osa/OSAFarmEnk.java
@@ -147,30 +147,35 @@ public class OSAFarmEnk extends OSABase {
               Pittango.answer(driver, sele3, wid);
             }
             else if ((cUrl.indexOf("cosmelife.com/animal") >= 0
+                || cUrl.indexOf("/animal/") >= 0
             //            || cUrl.indexOf("eyelashes-fashion.com") >= 0
             )
                 && isExistEle(driver, sele6)) {
               Zukan.answer(driver, sele6, wid);
             }
             else if ((cUrl.indexOf("cosmelife.com/observation") >= 0
+                || cUrl.indexOf("/observation/") >= 0
             //            || cUrl.indexOf("eyelashes-fashion.com") >= 0
             )
                 && isExistEle(driver, sele6)) {
               Kansatu.answer(driver, sele6, wid);
             }
             else if ((cUrl.indexOf("cosmelife.com/map") >= 0
+                || cUrl.indexOf("/map/") >= 0
             //            || cUrl.indexOf("eyelashes-fashion.com") >= 0
             )
                 && isExistEle(driver, sele6)) {
               Hyakkey.answer(driver, sele6, wid);
             }
             else if ((cUrl.indexOf("cosmelife.com/cooking") >= 0
+                || cUrl.indexOf("/cooking/") >= 0
             //                || cUrl.indexOf("eyelashes-fashion.com") >= 0
             )
                 && isExistEle(driver, sele6)) {
               Cooking.answer(driver, sele6, wid);
             }
             else if ((cUrl.indexOf("photo-enquete") >= 0
+                || cUrl.indexOf("/photo/") >= 0
                 || cUrl.indexOf("cosmelife.com/photo") >= 0
                 || cUrl.indexOf("eyelashes-fashion.com") >= 0
                 || cUrl.indexOf("natural-vegetables.com") >= 0
@@ -179,6 +184,7 @@ public class OSAFarmEnk extends OSABase {
               PhotoEnk.answer(driver, sele6, wid);
             }
             else if ((cUrl.indexOf("column-enquete") >= 0
+                || cUrl.indexOf("/column/") >= 0
                 || cUrl.indexOf("beautynail-design.com") >= 0
                 || cUrl.indexOf("style-cutehair.com") >= 0
                 || cUrl.indexOf("eyelashes-fashion.com") >= 0
