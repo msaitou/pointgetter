8df8959cb2984cc814b3524563aa070d36824fe0
saitou <saitou@VAIOsaito>
2018-04-29 17:44:40 +0900

    gpo修正

--- src/pointGet/mission/gpo/GPOFarmEnk.java
+++ src/pointGet/mission/gpo/GPOFarmEnk.java
@@ -128,24 +128,28 @@ public class GPOFarmEnk extends GPOBase {
               }
             }
             else if ((cUrl.indexOf("cosmelife.com/animal") >= 0
+                || cUrl.indexOf("/animal/") >= 0
                 //            || cUrl.indexOf("eyelashes-fashion.com") >= 0
                 )
                 && isExistEle(driver, sele6)) {
               Zukan.answer(driver, sele6, wid);
             }
             else if ((cUrl.indexOf("cosmelife.com/observation") >= 0
+                || cUrl.indexOf("/observation/") >= 0
                 //            || cUrl.indexOf("eyelashes-fashion.com") >= 0
                 )
                 && isExistEle(driver, sele6)) {
               Kansatu.answer(driver, sele6, wid);
             }
             else if ((cUrl.indexOf("cosmelife.com/map") >= 0
+                || cUrl.indexOf("/map/") >= 0
                 //            || cUrl.indexOf("eyelashes-fashion.com") >= 0
                 )
                 && isExistEle(driver, sele6)) {
               Hyakkey.answer(driver, sele6, wid);
             }
             else if ((cUrl.indexOf("cosmelife.com/cooking") >= 0
+                || cUrl.indexOf("/cooking/") >= 0
                 //                || cUrl.indexOf("eyelashes-fashion.com") >= 0
                 )
                 && isExistEle(driver, sele6)) {
@@ -153,6 +157,7 @@ public class GPOFarmEnk extends GPOBase {
             }
             else if ((cUrl.indexOf("photo-enquete") >= 0
                 || cUrl.indexOf("cosmelife.com/photo") >= 0
+                || cUrl.indexOf("/photo/") >= 0
                 || cUrl.indexOf("eyelashes-fashion.com") >= 0
                 || cUrl.indexOf("natural-vegetables.com") >= 0
                 || cUrl.indexOf("cosmeticsstyle.com") >= 0)
@@ -160,6 +165,7 @@ public class GPOFarmEnk extends GPOBase {
               PhotoEnk.answer(driver, sele6, wid);
             }
             else if ((cUrl.indexOf("column-enquete") >= 0
+                || cUrl.indexOf("/column/") >= 0
                 || cUrl.indexOf("beautynail-design.com") >= 0
                 || cUrl.indexOf("style-cutehair.com") >= 0
                 || cUrl.indexOf("eyelashes-fashion.com") >= 0
