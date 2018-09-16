2b89f7a8922e7a4771c480a2781fcba581138a23
user <user@saitoNote>
2018-09-16 14:12:22 +0900

    コミット漏れ対応

--- src/pointGet/mission/gpo/GPOPointResearch.java
+++ src/pointGet/mission/gpo/GPOPointResearch.java
@@ -12,7 +12,7 @@ import pointGet.common.Utille;
 import pointGet.mission.parts.AnswerPointResearch;
 
 public class GPOPointResearch extends GPOBase {
-  final String url = "http://www.gpoint.co.jp/";
+  final String url = "https://www.gpoint.co.jp/";
   WebDriver driver = null;
   /* アンケートクラス　ポイントサーチ */
   AnswerPointResearch PointResearch = null;
@@ -30,7 +30,7 @@ public class GPOPointResearch extends GPOBase {
     driver = driverAtom;
     Utille.url(driver, url, logg);
     String sele = "a[href='https://kotaete.gpoint.co.jp/']>span.navi-icon",
-        selector2 = "li.menu05>a";
+        selector2 = "li.menu11>a";
     String sele1 = "div.ui-control.type-fixed>a.ui-button";
     int skip = 1;
     if (isExistEle(driver, sele)) {
