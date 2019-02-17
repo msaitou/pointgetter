6599d76b73564e2331cbd1ce50d04e54393d1f77
saitou <saitou@VAIOsaito>
2019-02-17 21:51:38 +0900

    さっきの追加忘れ

---
+++ src/pointGet/mission/lfm/LFMQuizKentei.java
@@ -0,0 +1,92 @@
+package pointGet.mission.lfm;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import pointGet.common.Utille;
+import pointGet.mission.parts.AnswerQuiz;
+
+/**
+ *
+ * @author saitou
+ */
+public class LFMQuizKentei extends LFMBase {
+  final String url = "http://lifemedia.jp/game/";
+  AnswerQuiz Quiz = null;
+
+  /**
+   * @param logg
+   */
+  public LFMQuizKentei(Logger logg, Map<String, String> cProps) {
+    super(logg, cProps, "LFMクイズ検定");
+    Quiz = new AnswerQuiz(logg);
+  }
+
+  @Override
+  public void privateMission(WebDriver driver) {
+    Utille.url(driver, url, logg);
+    selector = "div>img[alt='クイズ検定Q']";
+    if (isExistEle(driver, selector)) {
+      clickSleepSelector(driver, selector, 6000); // 遷移
+      changeCloseWindow(driver);
+      Utille.sleep(9000);
+      //    String wid = wid0;
+
+      selector = "td.status>a"; // アンケート一覧の回答するボタン
+      Utille.sleep(5000);
+      String sele = "a>img.next_bt";
+      String overLay = "div#interstitial[style*='display: block']>div>div#inter-close";
+      checkOverlay(driver, overLay, false);
+      int skip = 0, beforeSize = 0;
+
+      while (true) {
+        if (isExistEle(driver, selector)) {
+          List<WebElement> eleList = driver.findElements(By.cssSelector(selector));
+          int size = eleList.size(), targetIndex = skip;
+          if (beforeSize == size) {
+            skip++;
+          }
+
+          logg.info("size:" + size + " target:" + targetIndex);
+          if (size > targetIndex && isExistEle(eleList, targetIndex)) { // 古い順にやる
+            Utille.scrolledPage(driver, eleList.get(targetIndex));
+            //            clickSleepSelector(driver, eleList, targetIndex, 5000); // アンケートスタートページ
+            Actions actions = new Actions(driver);
+            actions.keyDown(Keys.CONTROL);
+            actions.click(eleList.get(targetIndex));
+            actions.perform();
+            Utille.sleep(3000);
+
+            String wid = driver.getWindowHandle();
+            changeWindow(driver, wid);
+
+            if (isExistEle(driver, sele)) {
+              Quiz.answer(driver, sele, wid);
+              driver.close();
+              driver.switchTo().window(wid);
+            }
+            else {
+              driver.close();
+              driver.switchTo().window(wid);
+            }
+          }
+          else {
+            break;
+          }
+          beforeSize = size;
+          Utille.refresh(driver, logg);
+        }
+        else {
+          break;
+        }
+      }
+    }
+  }
+}
