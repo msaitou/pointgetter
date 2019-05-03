b7826e6109f8e2eff29cafe39c8fa1c81f9a6795
saitou <saitou@VAIOsaito>
2019-05-04 02:12:57 +0900

    LFMのぽっちっとの対応

--- src/pointGet/mission/lfm/LFMCMother.java
+++ src/pointGet/mission/lfm/LFMCMother.java
@@ -1,41 +1,39 @@
 package pointGet.mission.lfm;
 
-import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 import pointGet.common.Utille;
-import pointGet.mission.parts.AnswerSouSenkyo;
+import pointGet.mission.parts.AnswerDotti;
 
 public class LFMCMother extends LFMBase {
   final String url = "http://lifemedia.jp/game/";
   WebDriver driver = null;
-  AnswerSouSenkyo SouSenkyo = null;
+  AnswerDotti Dotti = null;
 
   /**
    * @param logg
    */
   public LFMCMother(Logger logg, Map<String, String> cProps) {
     super(logg, cProps, "CMそのた");
-    SouSenkyo = new AnswerSouSenkyo(logg);
+    Dotti = new AnswerDotti(logg);
   }
 
   @Override
   public void privateMission(WebDriver driverAtom) {
     driver = driverAtom;
     Utille.url(driver, url, logg);
-    String sele0 = "a.start__button" //
-    , sele1 = "ul.select__list>li>a" // クラスを完全一致にするのは済の場合クラスが追加されるため
-    , preSele = "img[src*='bn_sosenkyo.png']";
     selector = "img[alt='CMじゃんけん']";
+    String sele0 = "a.ui-btn.ui-btn-a" // アンケート一覧の回答するボタン
+    , sele1 = "ul.select__list>li>a" // クラスを完全一致にするのは済の場合クラスが追加されるため
+    ;
     if (isExistEle(driver, selector)) {
-      clickSleepSelector(driver, selector, 6000); // 遷移
+      clickSleepSelector(driver, selector, 5000); // 遷移
       changeCloseWindow(driver);
-      Utille.sleep(10000);
+      Utille.sleep(8000);
       // ポイントに変換
       String topSele = "a>img[src*='icon_game_off.png']";
       String bankSele = "li.icon_h_bank>a", coinSele = "p.h_coin>em", changeSele = "#modal-open.btn", changeBtnSele = "#modal-content2[style*='display: block;']>a.btn";
@@ -58,31 +56,30 @@ public class LFMCMother extends LFMBase {
         // 遊んで貯めるに移動
         clickSleepSelector(driver, topSele, 4000);
       }
-      String[] seles = { 
-          "p>img[src*='dotti2']",//
-//          "p>img[src*='column_']",//
-//          "p>img[src*='photo_']",//
-//          "p>img[src*='observation_']", //
-//          "p>img[src*='zoo_']", //
-//          "p>img[src*='japan_']",//
-//          "p>img[src*='food']" 
-          };
-
-      if (isExistEle(driver, preSele)) {
-        clickSleepSelector(driver, preSele, 5000); // 遷移 全体へ
-        changeCloseWindow(driver);
-        Utille.sleep(5000);
-        if (isExistEle(driver, sele0)) {
-          clickSleepSelector(driver, sele0, 5000); // 遷移 全体へ
-          while (isExistEle(driver, sele1)) {
-            List<WebElement> eleList = driver.findElements(By.cssSelector(sele1));
-            int size1 = eleList.size(), zumiCnt = 0, targetIndex = 0;
-            if (isExistEle(eleList, targetIndex)) {
-              Utille.scrolledPage(driver, eleList.get(targetIndex));
-              clickSleepSelector(driver, eleList, targetIndex, 4000); // 遷移
-              SouSenkyo.answer(driver, "", null);
+      String[] seles = { "p>img[src*='dotti2']",//
+      //          "p>img[src*='column_']",//
+          //          "p>img[src*='photo_']",//
+          //          "p>img[src*='observation_']", //
+          //          "p>img[src*='zoo_']", //
+          //          "p>img[src*='japan_']",//
+          //          "p>img[src*='food']" 
+      };
+      for (int i = 0; i < seles.length; i++) {
+        String currentSele = seles[i];
+        if (isExistEle(driver, currentSele)) {
+          clickSleepSelector(driver, currentSele, 5000); // 遷移 全体へ
+          String wid0 = driver.getWindowHandle();
+          changeWindow(driver, wid0);
+          Utille.sleep(5000);
+          String cUrl0 = driver.getCurrentUrl();
+          while (true) {
+            if ((cUrl0.indexOf("dotti2") >= 0)) {
+              Dotti.answer(driver, "", wid0);
+              break;
             }
             else {
+              driver.close();
+              driver.switchTo().window(wid0);
               break;
             }
           }
